cee194ff2c8d0db2381ad177238b4860bb7234ac
FKettl <fekettl@gmail.com>
2023-10-21 17:40:28 -0300

    Add initial db implementation and add gateway

--- db_manager.py
+++ db_manager.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
 import json
+
+
 app = Flask(__name__)
 
-
 db_path = 'db.json'
-in_memory_datastore = {
-   "Python" : {"name": "Python", "publication_year": 10960, "contribution": "record data"}
-}
 
 def write_json(path, json_data):
     with open(path, 'r+') as file_out:
@@ -20,22 +18,22 @@ def read_json(path):
     with open(path) as file_in:
         return json.load(file_in)
 
-@app.get('/orders/<int:id>')
+@app.get('/get_orders/<int:id>')
 def list_order_by_id(id):
   orders = read_json(db_path)
   for [k, v] in orders.items():
     if k == id:
        return v
 
-@app.get('/orders')
+@app.get('/get_orders')
 def get_order_by_id():
   orders = read_json(db_path)
   return orders
 
-@app.put('/orders/<int:id>')
+@app.post('/save_order/<int:id>')
 def write_order():
   order = request.get_json()
-  write_json(db_path, )
+  write_json(db_path, order)
 
 
 #write_json(db_path, in_memory_datastore)
@@ -43,4 +41,4 @@ def write_order():
 #print(response)
 if __name__ == '__main__':
   pass
-  #app.run(port=5000, host='localhost', debug=True)
+  #app.run(port=5001, host='localhost', debug=True)
---
+++ gateway.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, make_response, jsonify
+import json
+import requests
+
+app = Flask(__name__)
+cache = {}
+
+@app.post('/realizer-pedido')
+def create_order():
+  data = request.get_json();
+  order = {}
+  for [k, v] in data.items():
+    order[k] = v
+  cache[order['id']] = order
+  return make_response("Success", 201)
+
+@app.get('/<id>/get-cardapio')
+def get_menu(id):
+  return requests.get(url=f"http://localhost:5001/get-menu/{id}")
+  # make_response(jsonify(menu), 200)
+
+@app.get('/<int:id>/get-status-pedido')
+def get_status_pedido(id):
+  if id in cache:
+    return make_response(jsonify({'status':cache[id]['status']}), 200)
+  else:
+   return make_response('Order not found', 404)
+
+@app.post('/cadastrar-comida')
+def add_food():
+  return requests.post(url=f"http://localhost:5001/add-food/", data=request.get_json())
+
+@app.delete('/remover-comida')
+def remove_food():
+  return requests.post(url=f"http://localhost:5001/remove-food/", data=request.get_json())
+
+@app.put('/atualizar-status-pedido')
+def update_order_status():
+  data = request.get_json()
+  if data['id'] in cache:
+    cache['id'] = data['status']
+    return make_response("Success", 202)
+  else:
+    return make_response('Order not found', 404)
+
+if __name__ == '__main__':
+  pass
+  app.run(port=5000, host='localhost', debug=True)
