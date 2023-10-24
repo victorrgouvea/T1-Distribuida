0bbc1d98d07adac0864600140d15fc9c2e75e670
FKettl <fekettl@gmail.com>
2023-10-23 22:58:20 -0300

    Final changes

--- db_manager.py
+++ db_manager.py
@@ -4,10 +4,11 @@ import json
 app = Flask(__name__)
 
 db_path = 'db.json'
-restaurant_id = 3
+restaurant_id = 2
 
 @app.get('/get-highest-id/<key>')
 def get_highest_id(key):
+  print("chamou")
   highest_id = 0
   with open(db_path) as file_out:
     j = json.load(file_out)
@@ -58,21 +59,18 @@ def save_order():
         j = json.load(file_out)
         j['pedidos'][data['id']] = {}
         for [k, v] in data.items():
-            if k == 'id':
-                continue
             j['pedidos'][data['id']][k] = v
         file_out.seek(0)
         json.dump(j, file_out, indent=4)
   return make_response('Sucess', 200)
 
-@app.get('/get-menu/<name>')
-def get_menu(name):
+@app.get('/get-menu/<id>')
+def get_menu(id):
   data = None
-
   with open(db_path) as file_out:
         j = json.load(file_out)
         for [k, v] in j['restaurantes'].items():
-           if name == v['nome']:
+           if id == v['id']:
               data = j['restaurantes'][k]
   if data != None:
      return make_response(jsonify(data), 200)
@@ -95,22 +93,25 @@ def get_order_history():
     data = j['pedidos']
   return make_response(jsonify(data), 200)
 
-@app.get('/create-restaurant/<name>')
+@app.post('/create-restaurant/<name>')
 def create_restaurant(name):
-  if restaurant_id == 3:
-    restaurant_id = get_highest_id('restaurantes').get_json['highest_id'] + 1
+  global restaurant_id
+  if restaurant_id == 2:
+    restaurant_id = get_highest_id('restaurantes').json['highest_id'] + 1
   else:
     restaurant_id += 1
   j = None
+  print(restaurant_id)
   with open(db_path, 'r') as file_out:
     j = json.load(file_out)
-
   with open(db_path, 'w') as file_out:
-    j['restaurantes'][restaurant_id] = {}
-    j['restaurantes'][restaurant_id]['name'] = name
+    j['restaurantes'][str(restaurant_id)] = {}
+    j['restaurantes'][str(restaurant_id)]['nome'] = name
+    j['restaurantes'][str(restaurant_id)]['comidas'] = []
+    j['restaurantes'][str(restaurant_id)]['id'] = str(restaurant_id)
     file_out.seek(0)
     json.dump(j, file_out, indent=4)
-  return make_response('Sucess', 200)
+  return make_response(jsonify(j['restaurantes'][str(restaurant_id)]), 200)
 
 if __name__ == '__main__':
   app.run(port=5001, host='localhost', debug=True)
